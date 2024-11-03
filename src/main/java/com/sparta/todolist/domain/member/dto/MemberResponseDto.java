534fea0606c45481a8cbeb9e49e4a30ae681c9da
Younghan Yonn <dudgks1172@naver.com>
2024-11-03 12:40:53 +0900

    refactor: Member Dto 보완

--- src/main/java/com/sparta/todolist/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/sparta/todolist/domain/member/dto/MemberResponseDto.java
@@ -3,16 +3,20 @@ package com.sparta.todolist.domain.member.dto;
 import com.sparta.todolist.domain.member.entity.Member;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemberResponseDto {
     private Long id;
     private String name;
     private String email;
+    private LocalDateTime updatedAt;
 
 
     public MemberResponseDto(Member member) {
         this.id = member.getId();
         this.name = member.getName();
         this.email = member.getEmail();
+        this.updatedAt = member.getUpdatedAt();
     }
 }
